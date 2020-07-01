eba3f92ccb133a3df80cabc46e3e35ed142c59fe
thaddeusphua <thaddeus.phua@gmail.com>
2020-07-01 22:45:52 +0800

    added full player animations

--- player_controls/Assets/Scripts/Player.cs
+++ player_controls/Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
 
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    public Animator animator;
     Vector2 movement;
 
 
@@ -19,13 +20,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // get inputs
+        // get inputs from user
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        // this is used to update the player animation
+        // this is passed into the animator parameters, which is then used to update the blend tree
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        Debug.Log("dX: " + movement.x + " dY: " + movement.y + " dV: " + movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
+        // actual movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
