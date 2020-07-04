72b778c4a1ba96bc20e059b429f3008757c18a48
kim <kohseukim@gmail.com>
2020-07-04 22:51:01 +0800

    spawn monster  successful. location has issues atm

---
+++ test_monster/Assets/Scripts/Monster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Monster : MonsterType
+{   
+    public int amount{get; private set;}
+
+    public GameObject[] monsters;
+
+    private int index = 0;
+
+    public Monster(items type, int amount, float speed) : base(type, speed)
+    {
+        this.amount = amount;
+        monsters = new GameObject[amount];
+
+    }
+    
+    public void add(GameObject monster){
+        if (index < amount){
+           //set its index
+        //    monster.GetComponent<BasicObjectScript>().index = index;
+        //    monster.GetComponent<BasicObjectScript>().type = type;
+           monsters[index] = monster;
+           index ++;
+
+        }
+    }
+}
---
+++ test_monster/Assets/Scripts/MonsterType.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class MonsterType
+{
+    public float speed{get; private set;}
+
+    public items type{get; private set;}
+
+    public enum items {
+        GREENMONSTER = 0,
+    }
+
+
+    public MonsterType(items type, float speed){
+        this.type = type;
+        this.speed = speed;
+    }
+}
