f550f9c0b19a7417894f98c764216aa1a4539a7f
chiseng <chiseng_wong@mymail.sutd.edu.sg>
2020-07-06 10:40:39 +0800

    Working on movement

---
+++ player_controls/Assets/Scripts/Movement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Movement : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2d;
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    public LayerMask layerMask;
+
+    private float inverseMoveTime;
+
+    public float moveTime = 0.1f;
+    protected virtual void Start()
+    {
+        boxCollider = gameObject.GetComponent<BoxCollider2D>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+    }
+    // Start is called before the first frame update
+    public bool Move(int x, int y, out RaycastHit2D hit){
+        Vector2 start_position = transform.position;
+
+        Vector2 end_position = start_position + new Vector2(x, y);
+
+        boxCollider.enabled = false; //set current object's collider to be false so that it doesnt intefere with line
+
+
+        hit = Physics2D.Linecast(start_position, end_position, layerMask);
+
+        boxCollider.enabled = true;
+
+        if(hit.transform == null){
+            StartCoroutine(playerMovement(end_position));
+            return true;
+        }
+        return false;
+    }
+
+    IEnumerator playerMovement(Vector3 destination){
+        float sqrRemainingDistance = (transform.position - destination).sqrMagnitude;
+
+        while(sqrRemainingDistance > float.Epsilon){
+            rb2d.MovePosition(Vector3.MoveTowards(rb2d.position, destination, 30 * Time.deltaTime));
+            sqrRemainingDistance = (transform.position - destination).sqrMagnitude;
+            yield return null;
+        }
+    }
+
+    protected virtual void checkMove <T> (int x, int y) where T:Component{
+        RaycastHit2D hit;
+
+        bool move = Move(x, y, out hit);
+        if(hit.transform == null){
+            return;
+        }
+
+        T objectCollider = hit.transform.GetComponent<T>();
+
+        if(!move && objectCollider){
+            Debug.Log("Cant move!");
+            //change to something else maybe
+        }
+
+    }
+}
--- player_controls/Assets/Scripts/Player.cs
+++ player_controls/Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Movement
 {
 
     public float moveSpeed = 5f;
@@ -10,11 +10,19 @@ public class Player : MonoBehaviour
     public Animator animator;
     Vector2 movement;
 
+    private int xVal;
+
+    private int yVal;
+
+    private RaycastHit2D hit;
+    private BoxCollider2D walls;
+
 
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-
+        base.Start();
+        walls = GameObject.FindGameObjectWithTag("Wall").GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -24,17 +32,57 @@ public class Player : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        xVal = (int) movement.x;
+        yVal = (int) movement.y;
+
+        if(xVal != 0){
+            movement.y = 0.0f;
+            yVal = 0;
+            // checkMove<BoxCollider2D>(xVal, yVal);
+        }
+
+        if(yVal != 0){
+            movement.x = 0.0f;
+            xVal = 0;
+        }
+
+        if (xVal != 0 || yVal != 0){
+            checkMove<BoxCollider2D>(xVal, yVal);
+            
+        }
+
+        if(xVal == 0 && yVal == 0)
+        {
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+        }
         // this is used to update the player animation
         // this is passed into the animator parameters, which is then used to update the blend tree
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
-        Debug.Log("dX: " + movement.x + " dY: " + movement.y + " dV: " + movement.sqrMagnitude);
+        
+        
     }
 
-    void FixedUpdate()
+    // void FixedUpdate()
+    // {
+    //     // actual movement
+    //     rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    // }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    protected override void checkMove<T>(int x, int y)
     {
-        // actual movement
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        base.checkMove<T>(x,y);
+
+        if(Move(xVal, yVal, out hit)){
+            // base.Move(xVal, yVal, out hit);
+            Debug.Log("Moving");
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            Debug.Log(movement.sqrMagnitude);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+            Debug.Log("dX: " + movement.x + " dY: " + movement.y + " dV: " + movement.sqrMagnitude);
+            
+        }
     }
 }
