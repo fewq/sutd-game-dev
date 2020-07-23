27246cfa92407c9d3bd6a54d43d5cfc78af8744e
thaddeusphua <thaddeus.phua@gmail.com>
2020-07-23 18:49:49 +0800

    added HA&Kim's part

---
+++ player_controls/Assets/Scripts/PluggableAI/Action/ChaseAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Actions/Chase")]
+public class ChaseAction : Action
+{
+    private Transform target;
+    private Vector2 direction;
+    public override void Act(StateController controller)
+    {
+        Chase(controller);
+    }
+
+    private void Chase(StateController controller)
+    {
+        // TODO: add instance to player prefab
+        // target = Player.instance.transform;
+        // controller.monsterController.ChaseTarget(target);
+    }
+
+}
