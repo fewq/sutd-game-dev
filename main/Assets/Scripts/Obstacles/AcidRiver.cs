04ca87a3e3d9ecd8a358c2a61690d57e29a3a064
thaddeusphua <thaddeus.phua@gmail.com>
2020-08-09 01:06:53 +0800

    implemented death animation

--- main/Assets/Scripts/Obstacles/AcidRiver.cs
+++ main/Assets/Scripts/Obstacles/AcidRiver.cs
@@ -7,13 +7,13 @@ public class AcidRiver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,15 +21,16 @@ public class AcidRiver : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("DestroyPlayer");
-            GameManager.Instance.PlaySFX("playerscream");
-            GameManager.Instance.PlaySFX("acidriverkill");
+            // GameManager.Instance.PlaySFX("playerscream");
+            // GameManager.Instance.PlaySFX("acidriverkill");
             //Game over sequence here and destroy player object
-            //collision.gameObject.SetActive(false);
-            Destroy(collision.gameObject);
-            
-            GameObject.Find("GameManager").GetComponent<GameManager>().RestartGame();
+            // collision.gameObject.SetActive(false);
+            // Destroy(collision.gameObject);
 
-            
+            // GameObject.Find("GameManager").GetComponent<GameManager>().RestartGame();
+
+            // Coroutine for everything that is supposed to happen after a delay
+            StartCoroutine(PlayerDeath(collision));
         }
         if (collision.gameObject.CompareTag("Enemy"))
         {
@@ -38,4 +39,20 @@ public class AcidRiver : MonoBehaviour
         }
         //Kills enemy as well
     }
+
+    IEnumerator PlayerDeath(Collider2D collision)
+    {
+        GameManager.Instance.PlaySFX("playerscream");
+        GameManager.Instance.PlaySFX("acidriverkill");
+        // play the player death animation
+        collision.gameObject.GetComponent<Animator>().SetBool("Death", true);
+        // add delay before destroying player and restarting game
+        yield return new WaitForSeconds(1);
+        // restart game
+        collision.gameObject.SetActive(false);
+        Destroy(collision.gameObject);
+        GameObject.Find("GameManager").GetComponent<GameManager>().RestartGame();
+    }
 }
+
+
