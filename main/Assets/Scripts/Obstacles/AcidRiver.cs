a357cfa6883333d07187e99707a0801b9ac9e35f
ChongYiCheng <yicheng_chong@mymail.sutd.edu.sg>
2020-08-10 23:32:52 +0800

    cleaned up some code

--- main/Assets/Scripts/Obstacles/AcidRiver.cs
+++ main/Assets/Scripts/Obstacles/AcidRiver.cs
@@ -26,16 +26,6 @@ public class AcidRiver : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("DestroyPlayer");
-            // GameManager.Instance.PlaySFX("playerscream");
-            // GameManager.Instance.PlaySFX("acidriverkill");
-            //Game over sequence here and destroy player object
-            // collision.gameObject.SetActive(false);
-            // Destroy(collision.gameObject);
-            // GameObject.Find("GameManager").GetComponent<GameManager>().RestartGame();
-
-            // Coroutine for player death
-            // need this check. Coroutine or not, several sources of the sound will be played in parallel and sound distorted
-            // if (playerDeathCoroutineCheck == false) StartCoroutine(PlayerDeath(collision));
             collision.gameObject.GetComponent<Player>().playerDeath("acid");
 
         }
@@ -45,30 +35,9 @@ public class AcidRiver : MonoBehaviour
             Destroy(collision.gameObject);
             GameManager.Instance.PlaySFX("goblindeath");
         }
-        //Kills enemy as well
     }
 
-    // IEnumerator PlayerDeath(Collider2D collision)
-    // {
-    //     Debug.Log("Acid river playing PlayerDeath coroutine");
-    //     playerDeathCoroutineCheck = true;
-    //     // play audio
-    //     // stuff before restart
-    //     GameManager.Instance.PlaySFX("playerscream");
-    //     GameManager.Instance.PlaySFX("acidriverkill");
-    //     // play the player death animation
-    //     collision.gameObject.GetComponent<Animator>().SetBool("Death", true);
-    //     // add delay before destroying player and restarting game
-    //     yield return new WaitForSeconds(0.3f);
-    //     // show the death canvas
-    //     deathCanvas.GetComponent<Canvas>().enabled = true;
-    //     yield return new WaitForSeconds(0.7f);
-    //     // restart game
-    //     collision.enabled = true;
-    //     Destroy(collision.gameObject);
-    //     playerDeathCoroutineCheck = false;
-    //     GameObject.Find("GameManager").GetComponent<GameManager>().RestartGame();
-    // }
+
 }
 
 
