e24de9f09f73480e87f4b1bc27e533b2d41b0c60
noblekid96 <44386386+noblekid96@users.noreply.github.com>
2020-08-10 18:22:43 +0800

    dialogues for lvl 3,4,5 done

--- main/Assets/Scripts/DialogueScripts/DialogueTrigger.cs
+++ main/Assets/Scripts/DialogueScripts/DialogueTrigger.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class DialogueTrigger : MonoBehaviour
 {
+    public bool triggerAtStart;
     public Dialogue dialogue;
 
     public void TriggerDialogue(){
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    void Start(){
+        if (triggerAtStart){
+            TriggerDialogue();
+        }
     }
 }
