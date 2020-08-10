084649838276a5b69add40d407b5e54398f2499c
chiseng <chiseng_wong@mymail.sutd.edu.sg>
2020-08-10 12:33:31 +0800

    add animation for bomb

--- main/Assets/Scripts/Items/Bomb.cs
+++ main/Assets/Scripts/Items/Bomb.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     public float countdown = 2f;
+    Color startColor;
+    Color toChangeColor;
     // Start is called before the first frame update
     void Start()
     {
         transform.localScale = GameManager.Instance.gridScale;
+        startColor = this.GetComponent<SpriteRenderer>().color;
+        toChangeColor = this.GetComponent<SpriteRenderer>().color;
 
+    }
+    
+    IEnumerator flashingBomb(){
+        for(float t = 0.01f; t< 2.0f; t+= Time.deltaTime){
+            this.GetComponent<SpriteRenderer>().color = Color.Lerp(startColor, Color.red, Mathf.Min(1,t/2f));
+            yield return null;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         countdown -= Time.deltaTime;
-        
+        StartCoroutine(flashingBomb());
         if (countdown <= 0f)
         {
             Debug.Log("EKUSUPLOSION!");
